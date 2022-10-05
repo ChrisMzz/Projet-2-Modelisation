4cf1667f11ba0a8d48812d7b694d43922dc861e5
Christopher MAZZERBO <christopher.mazzerbo@gmail.com>
2022-10-05 11:15:56 +0200

    courbes de Bézier réussies sur 01 et 02

---
+++ Code Questions/projet2_02.py
@@ -0,0 +1,63 @@
+#%%
+from projet2_01 import*
+import numpy as np
+from  math import*
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+#from matplotlib.animation import FuncAnimation
+
+
+
+# Pour e : 
+cn = [(1,2), (2,2), (2,2), (3,2), (3,3), (3,3)]
+cn += [(2,3), (1,3), (1,3), (1,2), (1,1), (1,1)]
+cn += [(2,1), (3,1), (3,1), (3,1.5)]
+    
+
+cn1 = [(1,2), (2,2), (2,2), (3,2), (3,3), (3,3)]
+cn1 += [(2,3), (1,3), (1,3), (1,2), (1,1), (1,1)]
+c8 = [1.8,2]
+cn2 = [(3,1), (3,1), (3,1.5)]
+
+plt.clf()
+fig, ax = plt.subplots()
+N = 40
+
+def move(N):
+    for k in range(N):
+        c8[0] += 0.2/(N)
+        c8[1] -= 1/(N)
+        cn = cn1 + [(c8[0], c8[1])] + cn2
+        x = extract(bezier(cn,segment(1000)), 0)
+        y = extract(bezier(cn,segment(1000)), 1)
+        ax.plot(extract(cn,0), extract(cn,1), "-x", color=(0.8 + 0.2*k/N, 0.8 - 0.2*k/N, 0.8 - 0.4*k/N))
+    for k in range(N):
+        c8[0] += 0.2/(N)
+        c8[1] -= 1/(N)
+        cn = cn1 + [(c8[0], c8[1])] + cn2
+        x = extract(bezier(cn,segment(1000)), 0)
+        y = extract(bezier(cn,segment(1000)), 1)    
+        ax.plot(x,y, color=(0.8-0.4*k/N,0.2*k/N,1))
+    fig.savefig("images-02/c8_modifié.png", dpi=300, format="png")
+
+def swap(i):
+    plt.clf()
+    fig, ax = plt.subplots()
+    cn = [(1,2), (2,2), (2,2), (3,2), (3,3), (3,3)]
+    cn += [(2,3), (1,3), (1,3), (1,2), (1,1), (1,1)]
+    cn += [(2,1), (3,1), (3,1), (3,1.5)]
+    temp = cn[i]
+    cn[i] = cn[i+1]
+    cn[i+1] = temp
+    cs = [cn[i], cn[i+1]]
+    x = extract(bezier(cn,segment(1000)), 0)
+    y = extract(bezier(cn,segment(1000)), 1)    
+    ax.plot(x,y, color=(0.4,0.2,1))
+    ax.plot(extract(cn,0), extract(cn,1), "-x", color="b")
+    ax.plot(extract(cs,0), extract(cs,1), "-x", color="r")
+    fig.savefig(f"images-02/swap/c{i}c{i+1}_échangés.png", dpi=300, format="png")
+
+for i in range(15):
+    swap(i)
+
+
